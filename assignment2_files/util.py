df3333f6c86b3500ccf018c44040c8a7fdbe336e
Nicholas Serrano <34073804+nickroxcks@users.noreply.github.com>
2018-03-25 14:09:33 -0600

    Decompression finally works

--- assignment2_files/util.py
+++ assignment2_files/util.py
@@ -1,6 +1,5 @@
 # The functions in this file are to be implemented by students.
-from bitio import BitReader
-from bitio import BitWriter
+import bitio  # changed this cause i dont know why they didnt in the first place
 import huffman
 import time
 
@@ -10,7 +9,6 @@ def read_tree(bitreader):
     and construct and return the tree. When this function returns, the
     bit reader should be ready to read the next bit immediately
     following the tree description.
-
     Huffman trees are stored in the following format:
       * TreeLeaf is represented by the two bits 01, followed by 8 bits
           for the symbol at that leaf.
@@ -18,33 +16,33 @@ def read_tree(bitreader):
           is represented by the two bits 00.
       * TreeBranch is represented by the single bit 1, followed by a
           description of the left subtree and then the right subtree.
-
     Args:
       bitreader: An instance of bitio.BitReader to read the tree from.
-
     Returns:
       A Huffman tree constructed according to the given description.
     '''
-    tree = huffman.TreeBranch(True, True)
 
-    def re_tree():
+    def re_tree(bitreader):
         bit = bitreader.readbit()
         if bit == 1:
-            tree.left = re_tree()
-            tree.right = re_tree()
+            tree = huffman.TreeBranch(None, None)
+            tree.left = re_tree(bitreader)
+            tree.right = re_tree(bitreader)
+            return tree
         elif bit == 0:
-            bit = bitreader.readbit()
-            if bit == 1:
-                bits = bitreader.readbits(8)
-                print(hex(bits))
-                return (bits)
-            elif bit == 0:
+            anotherbit = bitreader.readbit()
+            if anotherbit == 0:
                 return huffman.TreeLeaf(None)
-        return tree
-    if bitreader.readbit() == 1:
-        return re_tree()
-    else:
-        return None
+            else:
+                leafval = bitreader.readbits(8)
+                return huffman.TreeLeaf(leafval)
+
+    tree = huffman.TreeBranch(None, None)
+    bitreader.readbit()
+    tree.left = re_tree(bitreader)
+    tree.right = re_tree(bitreader)
+
+    return tree
 
 
 def decode_byte(tree, bitreader):
@@ -52,16 +50,24 @@ def decode_byte(tree, bitreader):
     Reads bits from the bit reader and traverses the tree from
     the root to a leaf. Once a leaf is reached, bits are no longer read
     and the value of that leaf is returned.
-
     Args:
       bitreader: An instance of bitio.BitReader to read the tree from.
       tree: A Huffman tree.
-
     Returns:
       Next byte of the compressed bit stream.
     """
-
-    return 0b01010010
+    # if instance is built into python, and is used to check
+    # when we reach our leaf with the value inside it
+    if isinstance(tree, huffman.TreeBranch):
+        bit = bitreader.readbit()
+        if bit == 0:
+            tree = tree.left
+            return decode_byte(tree, bitreader)
+        elif bit == 1:
+            tree = tree.right
+            return decode_byte(tree, bitreader)
+    elif isinstance(tree, huffman.TreeLeaf):
+        return tree.value
 
 
 def decompress(compressed, uncompressed):
@@ -69,16 +75,13 @@ def decompress(compressed, uncompressed):
     read_tree function. Then use that tree to decode the rest of the
     stream and write the resulting symbols to the 'uncompressed'
     stream.
-
     Args:
       compressed: A file stream from which compressed input is read.
       uncompressed: A writable file stream to which the uncompressed
           output is written.
-
-``
     '''
-    br = BitReader(compressed)
-    #print(bin(br.readbits(700)),"what")
+
+    '''br = BitReader(compressed)
     tree = read_tree(br)
     print(tree.left.left.left.left.left)
     # encoded_table = huffman.make_encoding_table(tree)
@@ -101,23 +104,29 @@ def decompress(compressed, uncompressed):
     bw.writebit(0b0)
     bw.writebit(0b0)
     bw.writebit(0b1)
-    bw.writebit(0b0)
+    bw.writebit(0b0)'''
+    # initialize the reading and writing
+    reader = bitio.BitReader(compressed)
+    writer = bitio.BitWriter(uncompressed)
+    tree = read_tree(reader)
+
+    while 1:
+        byte = decode_byte(tree, reader)
+        if byte == None:  # means ending
+            break
+        else:
+            writer.writebits(byte, 8)
 
 
 def write_tree(tree, bitwriter):
     '''Write the specified Huffman tree to the given bit writer.  The
     tree is written in the format described above for the read_tree
     function.
-
     DO NOT flush the bit writer after writing the tree.
-
     Args:
       tree: A Huffman tree.
       bitwriter: An instance of bitio.BitWriter to write the tree to.
     '''
-    bw = BitWriter(bitwriter)
-
-    bw.writebits(0b111, 3)
 
     pass
 
@@ -128,9 +137,7 @@ def compress(tree, uncompressed, compressed):
     from the input stream 'uncompressed' and write it to 'compressed'.
     If there are any partially-written bytes remaining at the end,
     write 0 bits to form a complete byte.
-
     Flush the bitwriter after writing the entire compressed file.
-
     Args:
       tree: A Huffman tree.
       uncompressed: A file stream from which you can read the input.
